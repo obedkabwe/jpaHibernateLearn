95a997ce16be6448204870a41176c548e452bb54
obedkabwe <kabweobed@gmail.com>
2023-09-13 19:11:07 -0500

    configuration of JPA folders

---
+++ src/main/java/application/program.java
@@ -0,0 +1,30 @@
+package application;
+
+import domaine.Person;
+
+public class program {
+
+	public static void main(String[] args) {
+
+
+		
+		Person  p1 = new Person(1,"obed","kabwe@gmail.com");
+		Person  p2 = new Person(1,"obed","kabwe@gmail.com");
+		Person  p3 = new Person(1,"obed","kabwe@gmail.com");
+		
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
